2f83d2d2ed1029354ab6cf61eeb8e008ee086c02
Lara Alvarez <a21300631@ceti.mx>
2025-06-09 07:29:28 -0600

    variables env

--- manito/settings.py
+++ manito/settings.py
@@ -108,16 +108,15 @@ WSGI_APPLICATION = 'manito.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',        
-        'NAME': 'manito_db',  # Cambia esto por el nombre de tu base de datos
-        'USER': 'postgres',  # Tu usuario de PostgreSQL 
-        'PASSWORD': '140506',  # Tu contraseña de PostgreSQL
-        'HOST': 'localhost',  # Servidor local
-        'PORT': '5432',  # Puerto por defecto de PostgreSQL
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
