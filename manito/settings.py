c851ace1df377480825396ff4001edbc1562dae7
Lara Alvarez <a21300631@ceti.mx>
2025-06-09 02:28:59 -0600

    plan a

--- manito/settings.py
+++ manito/settings.py
@@ -44,7 +44,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Application definition
 
