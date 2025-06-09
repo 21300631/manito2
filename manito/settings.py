c9569426d88de0b84991b2bf2197874eae8313b0
Lara Alvarez <a21300631@ceti.mx>
2025-06-09 02:13:02 -0600

    quitar hash

--- manito/settings.py
+++ manito/settings.py
@@ -44,7 +44,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Application definition
 
@@ -177,7 +177,7 @@ USE_TZ = True
 # settings.py
 MEDIA_URL = '/media/'  # URL pública para medios
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Ruta absoluta al directorio media/
-        
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
