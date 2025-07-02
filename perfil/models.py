f8171c9f4e5a755aa884f3588497e814291530dd
Lara Alvarez <a21300631@ceti.mx>
2025-07-01 23:53:22 -0600

    insignia static

--- perfil/models.py
+++ perfil/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Insignia(models.Model):
     nombre = models.CharField(max_length=50)
-    imagen = models.ImageField(upload_to='insignias/')
+    imagen = models.CharField(max_length=50)
     
     def __str__(self):
         return self.nombre
